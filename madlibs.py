e5b44db032d6b207a5982fd314f7ba22cbd62807
Raymond <seah_raymond@hotmail.com>
2021-03-14 00:39:09 +0800

    added python projects

---
+++ madlibs.py
@@ -0,0 +1,17 @@
+# name="raymond"
+
+# print("i am " + name)
+# print("i am {}".format(name))
+# print(f"i am {name}")
+
+
+adj = input("Adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+famous_person = input("Farmous person: ")
+
+
+madlib = f"Pythong is so {adj}! i love to {verb1}. \
+    Stay {verb2} like you are {famous_person}"
+
+print(madlib)
